49f1b7433c9b4e08f27d7e2a86184c77240b03d4
Joakim Björklind SOFTK20P <joakim.bjorklind@centria.fi>
2020-09-23 13:20:10 +0300

    exercise 1.9

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,48 +6,53 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + bad + neutral
+  const average =  (good-bad)/all
+  const positive = (good/all)*100
   
-
 
     const increaseByOne = () => setGood(good + 1)
     const increaseByZero = () => setNeutral(neutral + 1)
     const decreaseByOne = () => setBad(bad + 1)
 
-    const Display = ({ good }) => {
-      return (
-        <div>Good {good}</div>
-      )
-    }
-    const Display2 = ({ neutral }) => {
-      return (
-        <div>Neutral {neutral}</div>
-      )
-    }
-    const Display3 = ({ bad }) => {
-      return (
-        <div>Bad {bad}</div>
-      )
-    }
+  
 
+    
 
   return (
     <div>
       <h1>Give feedback!!!!</h1>
-      <button onClick={increaseByOne}>
-        good
-      </button>
-      <button onClick={increaseByZero}> 
-        neutral
-      </button>
-      <button onClick={decreaseByOne}> 
-        bad
-      </button>
+      <Button handleClick={increaseByOne}text='good'/>
+      <Button handleClick={increaseByZero}text='neutral'/> 
+      <Button handleClick={decreaseByOne}text='bad'/>
+      
       <h2>Stats!!</h2>
-      <Display good={good}/>
-      <Display2 neutral={neutral}/>
-      <Display3 bad={bad}/>
+      <Statistics good={good}  neutral={neutral} bad={bad} all={good + neutral + bad} average={(good-bad)/all} positive={(good/all)*100 + '%'}/>
+      
+      
+      
     </div>
   )
 }
+const Statistics = (props) =>{
+ 
+  if ( props.all === 0 ){
+    return <p>No feedbakk given</p>
+  } 
+  return (
+    <div>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>All {props.all}</p>
+      <p>average {props.average}</p>
+      <p>posi {props.positive}</p>
+     </div>)
 
+  }
+  const Button = (props) => (
+    <button onClick={props.handleClick}>{props.text}
+      
+    </button>
+  )
 ReactDOM.render(<App />, document.getElementById('root'))
