687884f26690e578d077f1d86bd58d6718ec4907
Joakim Bjorklind <joakim.bjorklind@centria.fi>
2020-10-14 10:33:47 +0300

    frontend push

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -3,15 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-import axios from 'axios';
-import * as serviceWorker from './serviceWorker';
 
-
-const promise = axios.get('http://localhost:3001/persons')
-
-promise.then(response => {
-  console.log(response)
-})
 ReactDOM.render(
   
     <App />,
@@ -19,6 +11,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.unregister();
+
 
 
