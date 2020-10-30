091bb707581f7bf280fb4d5c3ef592a467116e8a
Joakim Bjorklind <joakim.bjorklind@centria.fi>
2020-10-30 10:19:18 +0200

    added middleware in app

--- part4/Blog/app.js
+++ part4/Blog/app.js
@@ -18,7 +18,10 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
