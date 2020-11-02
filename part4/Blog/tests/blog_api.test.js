640ddc77403d8300191665d209ac0f16da72d687
Joakim Bjorklind <joakim.bjorklind@centria.fi>
2020-11-02 11:50:00 +0200

    exercise 4.10 ok!!

--- part4/Blog/tests/blog_api.test.js
+++ part4/Blog/tests/blog_api.test.js
@@ -47,6 +47,33 @@ beforeEach(async () => {
     expect(response.body[0].id).toBeDefined()
   })
 
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+        title: 'valid blog',
+        author: "vb",
+        url: "www.validblog.com",
+        likes: 22
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogs = await Blog.find({})
+     
+    expect(blogs).toHaveLength(testBlogs.length + 1)
+    
+    const authors = blogs.map(aut => aut.author)
+  
+    expect(authors).toContain(
+      newBlog.author
+    )
+
+
+  })
+
   
   afterAll(() => {
     mongoose.connection.close()
