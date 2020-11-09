94b5526032401af1a11a2e25b1c9ed5ed68b0311
Joakim Bjorklind <joakim.bjorklind@centria.fi>
2020-11-09 10:44:17 +0200

    exercise 2.14 works!!

--- part2/countries/src/Components/Country.js
+++ part2/countries/src/Components/Country.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const iconStyle = {
@@ -6,6 +6,17 @@ const iconStyle = {
   }
 
 const Country = ({ country }) => {
+
+  const [weather, setWeather] = useState([])
+
+  useEffect(() => {
+      axios
+      .get(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${country.capital}`)
+      .then(response => {
+        setWeather(response.data.current)
+      })
+    
+  }, [country])
 
     return (
         <div>
@@ -19,6 +30,10 @@ const Country = ({ country }) => {
             {country.languages.map(l => <li key={l.iso639_1}>{l.name}</li>)}
           </ul>
           <img src={country.flag} style={iconStyle} alt={country.name} />
+          <h2>The weather in {country.capital}</h2>
+          <p>Current temperature {weather.temperature}&nbsp;&deg;C </p>
+          { weather.weather_icons ? weather.weather_icons.map((u, i) => <img key={i} src={u} alt={`Weather in ${country.capital}`} style={iconStyle} />) : '' }
+          <p>wind: { Math.floor(weather.wind_speed/3.6) } m/s direction {weather.wind_dir}</p>
           
           
         </div>
