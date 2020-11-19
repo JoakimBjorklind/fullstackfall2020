50e75522a2f323450721672c5fc293bed818bb88
Joakim Bjorklind <joakim.bjorklind@centria.fi>
2020-11-19 14:01:04 +0200

    exericse 6.13 ok

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,11 +1,11 @@
-const anecdotesAtStart = [
+/*const anecdotesAtStart = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+]*/
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -17,9 +17,9 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
+//const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
+const reducer = (state = [], action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
@@ -29,7 +29,9 @@ const reducer = (state = initialState, action) => {
      const anecdoteVote = state.find(ane => ane.id === id) 
      const votedAnecdote = { ...anecdoteVote, votes: anecdoteVote.votes + 1 }
      return state.map(anec => anec.id !== id ? anec : votedAnecdote)
-     
+
+    case 'INITIALIZE_ANECDOTES': 
+      return action.data
     case 'NEW_ANEC': {
       return [...state, asObject(action.data.content)]
     }
@@ -43,6 +45,13 @@ export const voteVote = (id) => {
   return {
     type: 'VOTE',
     data: { id }
+  }
+}
+
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'INITIALIZE_ANECDOTES',
+    data: anecdotes
   }
 }
 
