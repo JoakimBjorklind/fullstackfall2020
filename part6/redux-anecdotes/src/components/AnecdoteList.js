583d53f6c78895079a31dfe20e8c2b90a4cd114e
Joakim Bjorklind <joakim.bjorklind@centria.fi>
2020-11-20 12:03:48 +0200

    exercise 6.19 alpha version

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,27 +1,30 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { voteVote } from '../reducers/anecdoteReducer'
 import { displayNotification } from '../reducers/notificationReducer'
+//import anecdotes from '../services/anecdotes'
 
-const AnecdoteList = () => {
-    const anecdotes = useSelector(state => state.anecdotes)
-    const dispatch = useDispatch()
-    const filter = useSelector(state => state.filter)
+const AnecdoteList = (props) => {
+    //const anecdotes = useSelector(state => state.anecdotes)
+    //const dispatch = useDispatch()
+    //const filter = useSelector(state => state.filter)
     
     const vote = (anecdote) => {
      
-      dispatch(voteVote(anecdote))
-      dispatch(displayNotification(`'${anecdote.content}' just got a vote!`, 5))
+      //dispatch(voteVote(anecdote))
+      //dispatch(displayNotification(`'${anecdote.content}' just got a vote!`, 5))
+      props.voteVote(anecdote)
+      props.displayNotification(`'${anecdote.content}' just got a vote!`, 5)
       
       
     }
 
     
     
-    const filteringAnecdotes = () => {
+    const filteringAnecdotes = (filter, anecdotes) => {
 
       
-      let filteredAnecdotes = [...anecdotes].filter(anec =>
+      let filteredAnecdotes = anecdotes.filter(anec =>
       anec.content.toLowerCase().includes(filter.toLowerCase()))
       return filteredAnecdotes
       }
@@ -36,7 +39,7 @@ const AnecdoteList = () => {
 
     return (
         <div>
-          {sortAnecdotes(filteringAnecdotes()).map(anecdote =>
+          {sortAnecdotes(filteringAnecdotes(props.filter, props.anecdotes)).map(anecdote =>
             <div key={anecdote.id}>
               <div>
                 {anecdote.content}
@@ -50,4 +53,15 @@ const AnecdoteList = () => {
     )
 }
 
-export default AnecdoteList
+const mapStateToProps = (state) => {
+  return {
+    anecdotes: state.anecdotes,
+    filter: state.filter
+  }
+}
+
+const ConnectedAnecdoteList = connect(
+  mapStateToProps, {voteVote, displayNotification}
+)(AnecdoteList)
+
+export default ConnectedAnecdoteList
